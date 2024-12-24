5194ceac0e93d6aeed438d00fa9fe10d7d126ee5
999Ali999 <999alishafiei999@gmail.com>
2024-12-24 17:09:46 +0100

    Refactor ticket page to destructure params for improved readability and maintainability

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -3,20 +3,15 @@ import { notFound } from "next/navigation";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 
-// type TicketPageProps = {
-//   params: Promise<{
-//     ticketId: string;
-//   }>;
-// };
-
 type TicketPageProps = {
-  params: {
+  params: Promise<{
     ticketId: string;
-  };
+  }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const ticket = await getTicket(params.ticketId);
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
 
   if (!ticket) {
     notFound();
